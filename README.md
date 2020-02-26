e00569bbe64d060e38bc989d192595b25b6a46c2
sakai <sakai@e3sys.co.jp>
2020-02-26 10:28:20 +0900

    remove unused files

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ enable your Chrome extension to run on Developer mode.
 - chrome://extensions
 - click Load unpacked and select cloned directory
 
-push added extension button on toolbar and input some key to search.
--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -1,39 +1,27 @@
 
-(function() {
+(() => {
 
     let setting = {
         "shiftKey": false,
         "altKey": false,
         "ctrlKey": false,
         "keyCode": 13,
-    };
+    }
 
-    window.onload = ()  => {
-        chrome.storage.sync.get(['key'], (result) => {
-            if (result !== null) {
-                setting = result.key;
-            }
-        }); 
-    };
-
-    window.addEventListener('keypress', onKeyPress)
-
-    function onKeyPress(e) {
+    const onKeyPress = (e) => {
         const google = "https://www.google.co.jp"
         if (e.keyCode === setting.keyCode) {
             if (e.shiftKey !== setting.shiftKey) {
                 return;
             };
             if (e.altKey !== setting.altKey) {
-                return;
+                return
             }
             if (e.ctrlKey !== setting.ctrlKey) {
-                return;
+                return
             }
 
-            let selected = window.getSelection();
-            // console.log(selected);
-            // triggerEvent(selected.anchorNode, 'contextmenu');
+            let selected = window.getSelection()
             if (selected.toString() !== "") {
                 window.open(`${google}/search?q=${selected.toString()}`)
             }
@@ -41,17 +29,8 @@
         }
     }
 
-//     function triggerEvent(element, event) {
-//    if (document.createEvent) {
-//        console.log(element);
-//        // IE以外
-//        var evt = document.createEvent("HTMLEvents");
-//        evt.initEvent(event, true, true ); // event type, bubbling, cancelable
-//        return element.dispatchEvent(evt);
-//    } else {
-//        // IE
-//        var evt = document.createEventObject();
-//        retur element.fireEvent("on"+event, evt)
-//    }
-//}
+
+    window.addEventListener('keypress', onKeyPress)
+
 })()
+
